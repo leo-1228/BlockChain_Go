3904d80163b2218336f7ff154daecf79cb50c96e
leo-1228 <akiraito731@gmail.com>
2024-05-17 01:58:34 -0700

    add Deserialize function of Block struct

--- blockchain/block.go
+++ blockchain/block.go
@@ -24,6 +24,18 @@ func (b *Block) Serialize() []byte {
 	return res.Bytes()
 }
 
+func Deserialize(data []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+
+	err := decoder.Decode(&block)
+
+	Handle(err)
+
+	return &block
+}
+
 //----- --  ------- --- -
 
 type BlockChain struct {
