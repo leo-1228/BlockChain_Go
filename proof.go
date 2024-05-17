85ba6d5bba9108af2412bc328c3ee9ac639aafa7
leo-1228 <akiraito731@gmail.com>
2024-05-17 00:46:56 -0700

    add ToHex function

--- proof.go
+++ proof.go
@@ -1,6 +1,9 @@
 package blockchain
 
 import (
+	"bytes"
+	"encoding/binary"
+	"log"
 	"math/big"
 )
 
@@ -18,4 +21,13 @@ func NewProofOfWork(b *Block) *ProofOfWork {
 	pow := &ProofOfWork{b, target}
 
 	return pow
+}
+
+func ToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+	}
+	return buff.Bytes()
 }
