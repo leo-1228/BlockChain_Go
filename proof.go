d3646f9905fba69dadfe59e99450173b2be935c8
leo-1228 <akiraito731@gmail.com>
2024-05-17 00:46:03 -0700

    add NewProofOfWork function

--- proof.go
+++ proof.go
@@ -10,3 +10,12 @@ type ProofOfWork struct {
 	Block  *Block
 	Target *big.Int
 }
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-Difficulty))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
+}
